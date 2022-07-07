8a3448effb2928bf6c13edf93b819dba8d1f1f01
Chrissie Ugay <chrissie.anne.k.ugay@accenture.com>
2022-07-07 22:10:39 +0800

    challenge completion

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public Vector3 cubePos = new Vector3(3, 4, 1);
+    public Vector3 cubeScale = Vector3.one * 1.3f;
+    public Color cubeColor = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    public float cubeRotation = 10.0f;
+    public float rotationSpeed = 10.0f;
+
+    void Start()
+    {
+        transform.position = cubePos;
+        transform.localScale = cubeScale;
+        
+        Material material = Renderer.material;
+
+        material.color = cubeColor;
+    }
+    
+    void Update()
+    {
+        transform.Rotate(cubeRotation * Time.deltaTime, rotationSpeed, 100.0f);
+    }
+}
---
+++ Assets/WebglPreBuildProcessing.cs
@@ -0,0 +1,13 @@
+#if UNITY_EDITOR
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+public class WebglPreBuildProcessing : IPreprocessBuildWithReport
+{
+public int callbackOrder => 1;
+public void OnPreprocessBuild(BuildReport report)
+{
+System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/usr/bin/python3");
+}
+}
+#endif
